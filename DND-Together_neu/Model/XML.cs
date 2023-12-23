37176aa69b28e4ec3050dc4c1839f9a7d2497fa3
Dominik Beisecker <bluff99@web.de>
2023-12-23 18:24:12 +0100

    Xml Parser Save started

--- DND-Together_neu/Model/XML.cs
+++ DND-Together_neu/Model/XML.cs
@@ -16,21 +16,55 @@ namespace DND_Together_neu.Model
 
             return new TabControl[] { };
         }
-
-        public static void SaveTabControls(IEnumerable<UIElement> tabControls)
+        /// <summary>
+        /// Saves the parent TabControl into an XML File in the "Data"-Folder.
+        /// If no "Data"-Folder exists it creates a new one.
+        /// </summary>
+        /// <param name="tabControls">Parent TabControl where each Tab contains another TabControl with Tabs (pages)</param>
+        public static void SaveTabControls(TabControl tabControls)
         {
-            // Jeder übergeordnete Tab
-            foreach (UIElement uiElement in tabControls)
+            try
             {
-                // hat ein eigenes TabControl als Kind
-                TabControl tab = (TabControl) uiElement;
-                foreach(TabItem tabItem in tab.Items)
-                { 
-                    // welches mehrere Tabs 
-                    Debug.Print("Tab: " +  tabItem.Header);
+                string xml = "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
+                // Jeder übergeordnete Tab
+                foreach (TabControl uiElement in tabControls.Items)
+                {
+                    xml += "\n<Scene>";
+                    // hat ein eigenes TabControl als Kind
+                    TabControl tab = (TabControl)uiElement;
 
-                    // und diese jeweils eine eigene URL haben
+                    xml += "\n\t<Category>";
+                    foreach (TabItem tabItem in tab.Items)
+                    {
+                        xml += "\n\t\t<Category>";
+                        // welches mehrere Tabs 
+                        Debug.Print("Tab: " + tabItem.Header);
+
+                        // und diese jeweils eine eigene URL haben
+                        xml += "\n\t\t\t<Name>";
+                        xml += tabItem.Header.ToString();
+                        xml += "</Name>";
+
+                        xml += "\n\t\t\t<Pages>";
+
+                        foreach(TabItem page in tab.Items)
+                        {
+                            // TODO
+                        }
+
+                        xml += "\n\t\t\t</Pages>";
+
+                        xml += "\n\t\t</Category>";
+                    }
+                    xml += "\n\t</Categories>";
+
+                    xml += "\n</Scene>";
                 }
+            }
+            catch (InvalidCastException e)
+            {
+                Debug.Print(e.Message);
+                MessageBox.Show("Übergebenes TabControl beinhaltet falsche Items.", "Achtung!", MessageBoxButton.OK, MessageBoxImage.Error);
             }
         }
     }
