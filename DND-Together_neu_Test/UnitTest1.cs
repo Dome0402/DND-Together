c9d6bb70b86bf82b833de8b43f805f248474a5a3
Dominik Beisecker <bluff99@web.de>
2023-12-23 15:33:39 +0100

    Testprojekt erstellt

---
+++ DND-Together_neu_Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace DND_Together_neu_Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
