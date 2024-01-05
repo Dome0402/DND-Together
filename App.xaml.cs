d3c7b14f7ad398346e2bd3805b50f5b0c8ba40bd
Dominik Beisecker <bluff99@web.de>
2024-01-05 11:50:02 +0100

    Another try

---
+++ App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using DND_Together.MVVM.ViewModels;
+using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace DND_Together
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow = new MainWindow()
+            {
+                DataContext = new MainViewModel()
+            };
+            MainWindow.Show();
+
+            
+
+            base.OnStartup(e);
+        }
+    }
+
+}
