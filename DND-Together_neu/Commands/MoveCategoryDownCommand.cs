bebb1a9f18d3332ae4c04570f3912c74f58558ca
Dominik Beisecker <bluff99@web.de>
2024-02-27 07:44:19 +0100

    Move Category Down implemented

--- DND-Together_neu/Commands/MoveCategoryDownCommand.cs
+++ DND-Together_neu/Commands/MoveCategoryDownCommand.cs
@@ -1,9 +1,12 @@
-﻿using DND_Together.MVVM.ViewModels;
+﻿using DND_Together.MVVM.Model;
+using DND_Together.MVVM.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
 
 namespace DND_Together.Commands
 {
@@ -12,7 +15,53 @@ namespace DND_Together.Commands
         OverviewTabViewModel _overviewTabViewModel { get; set; }
         public override void Execute(object parameter)
         {
-            throw new NotImplementedException();
+            // Searching the index where the category is at
+            int foundIndex = -1;
+            for (int i = 0; i < _overviewTabViewModel.CategoryTabs.Count; i++)
+            {
+                if (_overviewTabViewModel.CategoryTabs[i].Header.ToString() == (string)parameter)
+                {
+                    foundIndex = i;
+                    break;
+                }
+            }
+
+            // If Category was not found
+            if (foundIndex == -1)
+            {
+                MessageBox.Show("Ein Fehler ist aufgetreten. Kategorie wurde nicht gefunden.", "Fehler", MessageBoxButton.OK, MessageBoxImage.Error);
+                return;
+            }
+
+            // If Category is already on bottom of the list
+            if (foundIndex == _overviewTabViewModel.CategoryTabs.Count - 1)
+            {
+                MessageBox.Show("Die Kategorie ist bereits ganz unten.", "Warnung", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+
+            // In any other case the category can be moved
+            List<TabItem> tabItems = new List<TabItem>();
+            List<Category> categories = new List<Category>();
+            for (int i = 0; i < _overviewTabViewModel.CategoryTabs.Count; i++)
+            {
+                if (i == foundIndex + 1)
+                {
+                    tabItems.Add(_overviewTabViewModel.CategoryTabs[foundIndex]);
+                    categories.Add(_overviewTabViewModel.Scene.Categories[foundIndex]);
+                    continue;
+                }
+                if (i == foundIndex)
+                {
+                    tabItems.Add(_overviewTabViewModel.CategoryTabs[i + 1]);
+                    categories.Add(_overviewTabViewModel.Scene.Categories[i + 1]);
+                    continue;
+                }
+                tabItems.Add(_overviewTabViewModel.CategoryTabs[i]);
+                categories.Add(_overviewTabViewModel.Scene.Categories[i]);
+            }
+            _overviewTabViewModel.CategoryTabs = tabItems;
+            _overviewTabViewModel.Scene.Categories = categories;
         }
 
         public MoveCategoryDownCommand(OverviewTabViewModel overviewTabViewModel)
